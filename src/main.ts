c8afcf2af61b0041b0b02e58cd53a619655dd988
jonathanluu0 <jonathanluu0@gmail.com>
2025-10-01 10:53:39 -0700

    electron vite TS React all works together

---
+++ src/main.ts
@@ -0,0 +1,36 @@
+import { app, BrowserWindow } from "electron";
+import * as path from "path";
+
+const isDev = !app.isPackaged; // true when running `electron .` with vite dev server
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 1000,
+    height: 700,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  if (isDev) {
+    mainWindow.loadURL("http://localhost:5173");
+    mainWindow.webContents.openDevTools();
+  } else {
+    mainWindow.loadFile(path.join(__dirname, "../renderer/index.html"));
+
+  }
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
---
+++ src/renderer/App.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function App() {
+  return <h1>Hello Electron + React + TS! IT FINALLY WORKS</h1>;
+}
